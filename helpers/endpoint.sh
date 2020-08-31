2342ae4e404a80f24d1fd6a1db0287841883764a
miles <miles1618@gmail.com>
2020-08-31 06:11:55 -0500

    UPdate build script

--- helpers/endpoint.sh
+++ helpers/endpoint.sh
@@ -2,8 +2,9 @@
 
 # Assuming you deployed with a 'serverless deploy | tee deploy.out' this script
 # will auto fill in the TODOS_ENDPOINT environment variable
-
+echo "ENDPOINT.sh - Getting endpoint from logs"
 node helpers/getEndpoint.js > .build/endpoint.out
 export TODOS_ENDPOINT=`cat .build/endpoint.out`
+echo "exprted: TODOS_ENDPOINT=${TODOS_ENDPOINT}"
 
 echo $TODOS_ENDPOINT
